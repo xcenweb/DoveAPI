be640352fc3adbaf97ff9bc57ec94da7cfbc21f8
一只咕鸽 <xcenweb@qq.com>
2022-10-19 22:24:57 +0800

    更新 dove.php

--- config/dove.php
+++ config/dove.php
@@ -11,7 +11,15 @@ return [
 
     // 非debug模式(生产环境)下错误返回形式 json/page
 	'pe_debug_mode' => 'json',
-
+	
+	// 设置生产环境下报错返回json、tpl文件位置
+	'pe_debug_mode_json_path' => ROOT_DIR.'vendor/xcenweb/doveapi-core/src/tpl/debug/pe_json.php',
+	'pe_debug_mode_page_path' => ROOT_DIR.'vendor/xcenweb/doveapi-core/src/tpl/debug/pe_page.tpl',
+	
+	// 不建议自定义修改。设置开发环境下报错返回json、tpl文件位置
+	'debug_mode_json_path' => ROOT_DIR.'vendor/xcenweb/doveapi-core/src/tpl/debug/json.php',
+	'debug_mode_page_path' => ROOT_DIR.'vendor/xcenweb/doveapi-core/src/tpl/debug/page.tpl',
+	
     // 是否开启debug-log记录
     'error_log'=> true,
 
